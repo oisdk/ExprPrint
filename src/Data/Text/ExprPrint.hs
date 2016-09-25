295136cc17333d23b7139ca9082ca24c19570780
oisdk <oisin.kidney@gmail.com>
2016-09-25 19:15:13 +0100

    some tests

--- src/Data/Text/ExprPrint.hs
+++ src/Data/Text/ExprPrint.hs
@@ -12,6 +12,61 @@ data ShowExpr t e = Lit t
                   | Binary Side Int e t e
                   deriving Functor
 
+-- | A function for properly printing an expression tree
+-- with minimal parentheses.
+-- >>> :{
+-- data Expr = Number Integer
+--           | Expr :+: Expr
+--           | Expr :*: Expr
+--           | Expr :^: Expr
+-- infixr 8 :^:
+-- infixl 6 :+:
+-- infixl 7 :*:
+-- :}
+--
+-- >>> :{
+-- instance Num Expr where
+--   (+) = (:+:)
+--   (*) = (:*:)
+--   fromInteger = Number
+--   abs = undefined
+--   signum = undefined
+--   negate = undefined
+-- :}
+--
+-- >>> :set -XOverloadedStrings
+-- >>> import Data.String
+-- >>> import Data.Monoid
+-- >>> :{
+-- newtype DiffStr = DiffStr { runDiffStr :: ShowS }
+-- instance Monoid DiffStr where
+--   mempty = DiffStr id
+--   DiffStr x `mappend` DiffStr y = DiffStr (x . y)
+-- instance IsString DiffStr where fromString = DiffStr . showString
+-- :}
+--
+-- >>> :{
+-- instance Show Expr where
+--   showsPrec _ = runDiffStr . showExpr (\s -> "(" <> s <> ")") proj where
+--     proj (Number n) = Lit (DiffStr (shows n))
+--     proj (x :+: y) = Binary L 3 x " + " y
+--     proj (x :*: y) = Binary L 4 x " * " y
+--     proj (x :^: y) = Binary R 5 x " ^ " y
+-- :}
+--
+-- >>> (1 + 2 + 3) :: Expr
+-- 1 + 2 + 3
+-- >>> (1 * 2 * 3) :: Expr
+-- 1 * 2 * 3
+-- >>> (1 * 2) + 3 :: Expr
+-- 1 * 2 + 3
+-- >>> 1 * (2 + 3) :: Expr
+-- 1 * (2 + 3)
+-- >>> (1 :^: 2) :^: 3
+-- (1 ^ 2) ^ 3
+-- >>> 1 :^: 2 :^: 3
+-- 1 ^ 2 ^ 3
+
 showExpr :: Monoid t
          => (t -> t)
          -> (e -> ShowExpr t e)
@@ -29,3 +84,4 @@ showExpr prns proj = rec . proj where
     Lit _ -> Nothing
     Unary s r _ _ -> Just (s,r)
     Binary s r _ _ _ -> Just (s,r)
+
